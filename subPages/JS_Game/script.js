34731e883ee3c0c2cb53019d717505b422e08c28
ItProgrammist <50521864+ItProgrammist@users.noreply.github.com>
2020-12-19 16:19:57 +0700

    Update script.js

--- subPages/JS_Game/script.js
+++ subPages/JS_Game/script.js
@@ -7,14 +7,13 @@ field.height= window.innerHeight;
 
 let player = {
     sprite: new Image(),
-    size: {x: 2, y:2},
+    size: {x: 100, y:100},
     pos: {x: 0, y: 0}
 }
 
 $(document).ready(function () {
     
-
-    player.sprite.src = "https://github.com/ItProgrammist/Pages/blob/master/img/player.png?raw=true";
+    player.sprite.src = "shatle.png";
     player.sprite.width = player.size.x;
     player.sprite.height = player.size.y;
     let bg = new Image(field.width, field.height);
@@ -28,13 +27,13 @@ $(document).ready(function () {
 
 
 function renderObject(sprite, posX, posY){
-    console.log(sprite)
+    //console.log(sprite)
     ctx.drawImage(sprite, posX, posY);
 }
 
 $(field).on("mousemove", function(e){
-    player.pos.x = e.offsetX - player.size.x/2;
-    player.pos.y = e.offsetY - player.size.y/2;
+    player.pos.x = e.offsetX - player.size.x;
+    player.pos.y = e.offsetY - player.size.y;
 });
 
 
